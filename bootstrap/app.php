b62773f32a22a0d35e00b8c8338600efe86b4d12
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2024-08-20 21:26:01 -0400

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$basePath = $_ENV['APP_BASE_PATH'] = $_SERVER['BASE_PATH'] ?? dirname(__DIR__);
+
+$cacheDir = $basePath.DIRECTORY_SEPARATOR.'bootstrap'.DIRECTORY_SEPARATOR.'cache';
+
+if (! file_exists($cacheDir)) {
+    mkdir($cacheDir, 0777, true);
+}
+
+return Application::configure($basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
